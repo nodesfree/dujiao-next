bf256f7039def32122a767d9dc1c068b27cb498f
nodesfree <laibaidu@youou159.serv00.net>
2026-09-07 17:13:08 +0800

    Update service.go

--- internal/modules/identity/adminauth/totp/application/service.go
+++ internal/modules/identity/adminauth/totp/application/service.go
@@ -19,7 +19,7 @@ import (
 )
 
 const (
-	totpIssuerDefault     = "Dujiao-Next"
+	totpIssuerDefault     = "Shop-TGaaaa"
 	totpPendingTTL        = 10 * time.Minute
 	totpEnableMaxFailures = 5
 	RecoveryCodeCount     = 10
